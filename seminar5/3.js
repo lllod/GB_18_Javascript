7d31a63760f80acbcd942825ef825c16e3fb9528
lllod <ygo.admin@gmail.com>
2024-04-01 19:02:08 +0400

    added seminar5

---
+++ seminar5/3.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/*
+Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя
+метод forEach.
+Обратите внимание, что в массиве должны быть изменены цены продуктов после 
+выполнения метода forEach. Не нужно создавать новый массив.
+Если сложно работать с методами массива, то можно сделать и обычным циклом.
+*/
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(function (element, index) {
+    products[index].price = element.price - element.price * 0.15;
+})
+
+for (let productsKey in products) {
+    console.log(products[productsKey].price);
+}
